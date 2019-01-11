7149c4fef62539f164bff9d5ba8fafaeb3b47e6b
M. Hogeveen <mhogeveen94@gmail.com>
2019-01-11 18:45:14 +0100

    Auto scroll added to review wheel

--- public/resources/javascript/main.js
+++ public/resources/javascript/main.js
@@ -2,38 +2,47 @@ $(document).ready(function() {
 
   $('.btn1').click(function() {
     $('.rev1').animate({'left': '15%'});
-    $('.rev2').animate({'left': '100%'});
-    $('.rev3').animate({'left': '100%'});
-    $('.btn1').removeClass('active inactive');
+    $('.rev2,.rev3').animate({'left': '100%'});
+    $('.btn1,.btn2,.btn3').removeClass('active inactive');
     $('.btn1').addClass('active');
-    $('.btn2').removeClass('active inactive');
-    $('.btn2').addClass('inactive');
-    $('.btn3').removeClass('active inactive');
-    $('.btn3').addClass('inactive');
+    $('.btn2,.btn3').addClass('inactive');
   });
 
   $('.btn2').click(function() {
     $('.rev1').animate({'left': '-100%'});
     $('.rev2').animate({'left': '15%'});
     $('.rev3').animate({'left': '100%'});
-    $('.btn1').removeClass('active inactive');
-    $('.btn1').addClass('inactive');
-    $('.btn2').removeClass('active inactive');
+    $('.btn1,.btn2,.btn3').removeClass('active inactive');
+    $('.btn1,.btn3').addClass('inactive');
     $('.btn2').addClass('active');
-    $('.btn3').removeClass('active inactive');
-    $('.btn3').addClass('inactive');
   });
 
   $('.btn3').click(function() {
-    $('.rev1').animate({'left': '-100%'});
-    $('.rev2').animate({'left': '-100%'});
+    $('.rev1,.rev2').animate({'left': '-100%'});
     $('.rev3').animate({'left': '15%'});
-    $('.btn1').removeClass('active inactive');
-    $('.btn1').addClass('inactive');
-    $('.btn2').removeClass('active inactive');
-    $('.btn2').addClass('inactive');
-    $('.btn3').removeClass('active inactive');
+    $('.btn1,.btn2,.btn3').removeClass('active inactive');
+    $('.btn1,.btn2').addClass('inactive');
     $('.btn3').addClass('active');
   });
+
+  $('.btn1').trigger('click');
+
+  setTimeout(function() {
+    setInterval(function() {
+      $('.btn1').trigger('click');
+    }, 30000);
+  }, 0);
+
+  setTimeout(function() {
+    setInterval(function() {
+      $('.btn2').trigger('click');
+    }, 30000);
+  }, 10000);
+
+  setTimeout(function() {
+    setInterval(function() {
+      $('.btn3').trigger('click');
+    }, 30000);
+  }, 20000);
 
 });
