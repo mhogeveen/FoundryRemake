32fd2d1d1851e8eadb1a08b5a2c801af960ab8fd
M. Hogeveen <mhogeveen94@gmail.com>
2019-01-11 17:09:16 +0100

    Review slider/wheel complete

--- public/resources/javascript/main.js
+++ public/resources/javascript/main.js
@@ -1,5 +1,39 @@
 $(document).ready(function() {
 
-  
+  $('.btn1').click(function() {
+    $('.rev1').animate({'left': '15%'});
+    $('.rev2').animate({'left': '100%'});
+    $('.rev3').animate({'left': '100%'});
+    $('.btn1').removeClass('active inactive');
+    $('.btn1').addClass('active');
+    $('.btn2').removeClass('active inactive');
+    $('.btn2').addClass('inactive');
+    $('.btn3').removeClass('active inactive');
+    $('.btn3').addClass('inactive');
+  });
+
+  $('.btn2').click(function() {
+    $('.rev1').animate({'left': '-100%'});
+    $('.rev2').animate({'left': '15%'});
+    $('.rev3').animate({'left': '100%'});
+    $('.btn1').removeClass('active inactive');
+    $('.btn1').addClass('inactive');
+    $('.btn2').removeClass('active inactive');
+    $('.btn2').addClass('active');
+    $('.btn3').removeClass('active inactive');
+    $('.btn3').addClass('inactive');
+  });
+
+  $('.btn3').click(function() {
+    $('.rev1').animate({'left': '-100%'});
+    $('.rev2').animate({'left': '-100%'});
+    $('.rev3').animate({'left': '15%'});
+    $('.btn1').removeClass('active inactive');
+    $('.btn1').addClass('inactive');
+    $('.btn2').removeClass('active inactive');
+    $('.btn2').addClass('inactive');
+    $('.btn3').removeClass('active inactive');
+    $('.btn3').addClass('active');
+  });
 
 });
