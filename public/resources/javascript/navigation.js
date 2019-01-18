8521eab68f2cc80a53c81dafb94967664a35ddcf
M. Hogeveen <mhogeveen94@gmail.com>
2019-01-18 13:18:07 +0100

    Add js for screen width < 600px

--- public/resources/javascript/navigation.js
+++ public/resources/javascript/navigation.js
@@ -1,14 +1,30 @@
 $(document).ready(function() {
 
   // Change the background color of the navigation section after scrolling down a set amount
+  // $(window).scroll(function() {
+  //   if ($(window).scrollTop() > $('#banner-section').height()) {
+  //     $('#navigation-section').css('border-bottom', '1px solid lightgray');
+  //     $('#navigation-section').css('background-color', 'white');
+  //   }
+  //   if ($(window).scrollTop() < $('#banner-section').height()) {
+  //     $('#navigation-section').css('border-bottom', '1px solid rgba(255, 255, 255, 0)');
+  //     $('#navigation-section').css('background-color', 'rgba(255, 255, 255, 0)');
+  //   }
+  // });
+
   $(window).scroll(function() {
-    if ($(window).scrollTop() > $('#banner-section').height()) {
+    if ($(window).width() > 600) {
+      if ($(window).scrollTop() > $('#banner-section').height()) {
+        $('#navigation-section').css('border-bottom', '1px solid lightgray');
+        $('#navigation-section').css('background-color', 'white');
+      }
+      if ($(window).scrollTop() < $('#banner-section').height()) {
+        $('#navigation-section').css('border-bottom', '1px solid rgba(255, 255, 255, 0)');
+        $('#navigation-section').css('background-color', 'rgba(255, 255, 255, 0)');
+      }
+    } else {
       $('#navigation-section').css('border-bottom', '1px solid lightgray');
       $('#navigation-section').css('background-color', 'white');
-    }
-    if ($(window).scrollTop() < $('#banner-section').height()) {
-      $('#navigation-section').css('border-bottom', '1px solid rgba(255, 255, 255, 0)');
-      $('#navigation-section').css('background-color', 'rgba(255, 255, 255, 0)');
     }
   });
 
