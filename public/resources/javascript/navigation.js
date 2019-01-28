1d340a8f1d0276db8cbcb33ab67f888bd0401f79
M. Hogeveen <mhogeveen94@gmail.com>
2019-01-28 12:56:20 +0100

    Switch mobile nav from opacity to display menu toggle, add click outside menu to close menu

--- public/resources/javascript/navigation.js
+++ public/resources/javascript/navigation.js
@@ -29,23 +29,31 @@ $(document).ready(function() {
     var windowSize = $(window).width();
     if (windowSize > 600) {
       $('#navigation-section').css({'border-bottom': '1px solid rgba(255, 255, 255, 0)', 'background-color': 'rgba(255, 255, 255, 0)'});
-      $('.navigation-container ul').css('opacity', '1');
+      $('.navigation-container ul').css('display', 'flex');
       $('.navigation-container i').removeClass('fa-bars fa-times');
       $('.navigation-container i').addClass('fa-bars');
     } else if (windowSize <= 600) {
       $('#navigation-section').css({'border-bottom': '1px solid lightgray', 'background-color': 'white'});
-      $('.navigation-container ul').css('opacity', '0');
+      $('.navigation-container ul').css('display', 'none');
     }
   });
 
   // Show mobile view navigation menu on click of 'hamburger' icon
   $('.menu-button').click(function() {
     $('.navigation-container i').toggleClass('fa-bars fa-times');
-     if ($('.navigation-container ul').css('opacity') == '0') {
-       $('.navigation-container ul').animate({'opacity': '1'});
-     } else if ($('.navigation-container ul').css('opacity') == '1') {
-       $('.navigation-container ul').animate({'opacity': '0'});
+     if ($('.navigation-container ul').css('display') == 'none') {
+       $('.navigation-container ul').css('display', 'flex');
+     } else if ($('.navigation-container ul').css('display') == 'flex') {
+       $('.navigation-container ul').css('display', 'none');
      }
+  });
+
+  // Close menu if clicked outside of menu in mobile view
+  $('.main-content').click(function() {
+    if ($('.navigation-container ul').css('display') == 'flex') {
+      $('.navigation-container ul').css('display', 'none');
+      $('.navigation-container i').toggleClass('fa-bars fa-times');
+    }
   });
 
 });
